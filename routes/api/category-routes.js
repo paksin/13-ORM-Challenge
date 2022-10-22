118a282454816303e503f4b4a04e3e8634b23ae5
Pak Yin Sin <Spy@MacBook-Pro.local>
2022-10-21 23:27:07 -0400

    code mostly completed. product API has bug.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const cateData = await Category.findAll();
+    res.status(200).json(cateData);
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try{
+    const cateData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    if(!cateData) {
+      res.status(404).json({
+        message: 'No location found with that id!'
+    });
+    return;
+    }
+
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const cateData = await Category.create(req.body);
+    res.status(200).json(cateData);
+} catch (err) {
+    res.status(400).json(err);
+}
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const cateData = await Category.update(req.body, {where: {id: req.params.id}});
+    res.status(200).json(cateData);
+} catch (err) {
+    res.status(400).json(err);
+}
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const cateData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!cateData) {
+      res.status(404).json({ message: 'No traveller found with that id!' });
+      return;
+    }
+
+    res.status(200).json(cateData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
