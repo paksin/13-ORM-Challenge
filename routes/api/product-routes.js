ae9f7677bd61c0db7b0ddb6047fb565726d7b4a9
Pak Yin Sin <Spy@MacBook-Pro.local>
2022-10-21 23:33:07 -0400

    bug fixed. code complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const {
+  Product,
+  Category,
+  Tag,
+  ProductTag
+} = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -8,27 +13,41 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{
+        model: Category
+      }, {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags'
+      }],
+    });
     res.status(200).json(productData);
-} catch (err) {
+  } catch (err) {
     res.status(500).json(err);
-}
+  }
 });
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  try{
+  try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category}, { model: Tag, through: ProductTag, as: 'tags'}],
+      include: [{
+        model: Category
+      }, {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags'
+      }],
     });
 
-    if(!productData) {
+    if (!productData) {
       res.status(404).json({
         message: 'No location found with that id!'
-    });
-    return;
+      });
+      return;
     }
 
     res.status(200).json(productData);
@@ -73,17 +92,23 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
+      where: {
+        id: req.params.id,
+      },
+    })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({
+        where: {
+          product_id: req.params.id
+        }
+      });
     })
     .then((productTags) => {
       // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      const productTagIds = productTags.map(({
+        tag_id
+      }) => tag_id);
       // create filtered list of new tag_ids
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
@@ -95,12 +120,20 @@ router.put('/:id', (req, res) => {
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+        .filter(({
+          tag_id
+        }) => !req.body.tagIds.includes(tag_id))
+        .map(({
+          id
+        }) => id);
 
       // run both actions
       return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.destroy({
+          where: {
+            id: productTagsToRemove
+          }
+        }),
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
@@ -121,7 +154,9 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No traveller found with that id!' });
+      res.status(404).json({
+        message: 'No traveller found with that id!'
+      });
       return;
     }
 
